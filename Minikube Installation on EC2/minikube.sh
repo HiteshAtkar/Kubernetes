d410bff2bdf2dd1fcb510dbd8444fd6a8595b701
Hitesh Atkar <113357395+HiteshAtkar@users.noreply.github.com>
2025-08-10 11:08:51 +0530

    Update minikube.sh

--- Minikube Installation on EC2/minikube.sh
+++ Minikube Installation on EC2/minikube.sh
@@ -11,7 +11,6 @@ sudo apt install -y curl wget apt-transport-https ca-certificates conntrack
 echo "Installing Docker..."
 sudo apt install -y docker.io
 sudo systemctl enable docker --now
-sudo usermod -aG docker $USER && newgrp docker
 
 echo "Downloading Minikube..."
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
@@ -23,5 +22,9 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
 
+echo "Adding current user to docker group..."
+sudo usermod -aG docker $USER
+
 echo "Installation complete!"
-echo "You may need to log out and log back in for Docker group changes to take effect."
+echo "Switching to new Docker group..."
+newgrp docker
